4226709bc5c8c85356008e599d49ddf0760af9d2
eternalfoxglove <yao.tong2018@outlook.com>
2025-01-25 12:10:14 +0000

    Coded tutorial Bubble Gun script

--- Assets/Scripts/BubbleGunTut.cs
+++ Assets/Scripts/BubbleGunTut.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class BubbleGun : MonoBehaviour
 {
+    public Transform parent; 
+    public GameObject prefab;
+    public GameObject self; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,13 @@ public class BubbleGun : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            Instantiate(prefab, parent);
+            Destroy(self);
+        }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject enemyPrefab; 
+    // Defines positions where enemies will spawn 
+    public Transform[] spawnPoints; 
+    public Transform player; 
+    public float spawnDelay = 2f;
+    public float spawnRadius = 20f; 
 
+    private float nextSpawnTime;  
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > nextSpawnTime && Vector3.Distance(player.position, transform.position) < spawnRadius) {
+            SpawnEnemy(); 
+            nextSpawnTime = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnEnemy() 
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length); 
+        Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
     }
 }
