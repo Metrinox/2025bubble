08cc2d9e1004f4b6d4d807e525ff7d83eb486e73
everythingfades <raymondguhonglei@163.com>
2025-01-25 11:09:02 +0000

    feat: add fish, fish can chase bubble and sea level settings

---
+++ Assets/Scripts/bounsBubble.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BounsBubble : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float size = 2.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
